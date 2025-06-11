34dd4b6b8757fe83fd6320d2abce8b75af5d38e6
brian070069 <kkipngeno409@gmail.com>
2025-06-12 00:25:34 +0300

    Using the BCrypt Password Encoder

---
+++ SpringSecurity/src/main/java/com/kipngeno/SpringSecurity/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.kipngeno.SpringSecurity.controller;
+
+import com.kipngeno.SpringSecurity.model.Users;
+import com.kipngeno.SpringSecurity.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping("/register")
+    public Users registerUser(@RequestBody Users user) {
+        return service.register(user);
+    }
+}
---
+++ SpringSecurity/src/main/java/com/kipngeno/SpringSecurity/service/UserService.java
@@ -0,0 +1,21 @@
+package com.kipngeno.SpringSecurity.service;
+
+import com.kipngeno.SpringSecurity.model.Users;
+import com.kipngeno.SpringSecurity.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+
+    public Users register(Users user) {
+        user.setPassword(encoder.encode(user.getPassword()));
+        return repo.save(user);
+    }
+}
